b3c79405364d7499d8c91d80cf421942bf16e87e
Eddie Englund <properllerbucher@gmail.com>
2021-06-06 19:09:02 +0200

    prepared nuxt project

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'Sofia Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 1em;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
